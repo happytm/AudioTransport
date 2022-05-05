662482ddfc51acd34653c8aed36bf8965a68750a
happytm <36576353+happytm@users.noreply.github.com>
2022-05-05 18:24:54 -0400

    Update Codec2_timing.ino

--- Codec2_timing.ino
+++ Codec2_timing.ino
@@ -19,32 +19,9 @@ void setup() {
   // Set bitrate
   codec2_state = codec2_create(CODEC2_MODE_1600);
   // Set some tuning parameters
-  codec2_set_lpc_post_filter(codec2_state, 1, 0, 0.8, 0.2);
-
+  codec2_set_lpc_post_filter(codec2_state, 1, 0, 0.8, 0.2) 
   
   xTaskCreate(&codec2_watcher, "codec2_watcher_task", 30000, NULL, 5, NULL);
-
-  // Encode a (fixed size) input buffer of type int16_t to a (fixed size) output buffer
-  // CODEC2_MODE_1600 encodes 320 speech samples (320 * 2 = 640 bytes / 40ms of speech) into 8 bytes (64 bits)
-  // Fill buffer with a sine wave
-  for (int i = 0; i < 320; i++) audioBuf[i] = sine1KHz[i % 8];
-  
-  for (int i = 0; i < 320; i++) Serial.print(String(audioBuf[i], DEC) + " ");
-  Serial.println();
-  
-  int startTimeEncode = millis();
-  c2_encode();
-  Serial.println("Done encoding, took ms: " + String(millis() - startTimeEncode));
-
-  for (int i = 0; i < 8; i++) Serial.print(String(c2Buf[i], DEC) + " ");
-  Serial.println();
-  
-  int startTimeDecode = millis();
-  c2_decode();
-  Serial.println("Done decoding, took ms: " + String(millis() - startTimeDecode));
-
-  for (int i = 0; i < 320; i++) Serial.print(String(audioBuf[i], DEC) + " ");
-  Serial.println();
 }
 
 void loop() {
